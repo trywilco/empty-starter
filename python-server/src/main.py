d412223ceb278945eb0bc393d613d35b9461157f
Shem <smagnezi8@gmail.com>
2024-07-17 14:29:50 +0300

    Fix routes

--- python-server/src/main.py
+++ python-server/src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Task(BaseModel):
+    text: str
 
 tasks = ["Write a diary entry from the future", "Create a time machine from a cardboard box", "Plan a trip to the dinosaurs", "Draw a futuristic city", "List items to bring on a time-travel adventure"]
 
@@ -8,9 +12,9 @@ tasks = ["Write a diary entry from the future", "Create a time machine from a ca
 def get_tasks():
     return "Hello World"
 
-@app.post("/task")
-def add_task(task: str):
-    tasks.append(task)
+@app.post("/tasks")
+def add_task(task: Task):
+    tasks.append(task.text)
     return {"message": "Task added successfully"}
 
 @app.get("/tasks")
